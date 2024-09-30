2e2408329c43f19e68bdaa5d91d663dfab7079d3
Dylan Wishon <dylanl.wishon@gmail.com>
2024-09-30 14:50:16 -0500

    Cors Test

--- server/main.py
+++ server/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI 
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+class Item(BaseModel):
+    hello: str
 
 app = FastAPI()
 
-@app.get("/")
-def test():
-    return {"hello": "world"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.post("/test/")
+async def test(item: Item):
+    print(item)
+    return item
