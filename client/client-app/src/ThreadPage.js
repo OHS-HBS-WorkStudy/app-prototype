358b9f689369dd7a89d7ff57c5d8e3d907bc90ef
Matthew Gray <matthew.riley.gray@outlook.com>
2024-10-30 14:03:06 -0500

    create reply function in client

--- client/client-app/src/ThreadPage.js
+++ client/client-app/src/ThreadPage.js
@@ -1,3 +1,6 @@
+import Navigator from "./modules/Navigator.js";
+import ThreadReply from "./modules/ThreadReply.js";
+
 export default function ThreadPage() {
     let data = JSON.parse(sessionStorage.getItem("thread"))
 
@@ -5,8 +8,10 @@ export default function ThreadPage() {
 
     return(
         <div className="Thread-Content">
+            <Navigator />
             <h1 className="Thread-Title">{data[0]}</h1>
             <p className="Thread-Desc">{data[1]}</p>
+            <ThreadReply />
         </div>
     )
 }
