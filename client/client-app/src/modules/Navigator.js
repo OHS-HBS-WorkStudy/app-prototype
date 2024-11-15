e832569c1eb3e0b4dedaee95d1a63b0820d43ec3
Dylan Wishon <dylanl.wishon@gmail.com>
2024-11-15 15:01:45 -0600

    don't work styles

--- client/client-app/src/modules/Navigator.js
+++ client/client-app/src/modules/Navigator.js
@@ -1,11 +1,11 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import {ScreenContext, ScreenStateContext} from '../App.js';
 import SearchBar from './SearchBar.js';
 import '../App.css';
 
 export default function Navigator() {
-    const bare = useContext(ScreenContext);
     const changeScreen = useContext(ScreenStateContext);
+
 
     function SignUp() {
         changeScreen(1);
@@ -27,11 +27,18 @@ export default function Navigator() {
         changeScreen(0);
     }
 
+
+
+
     return (
         <div>
             <nav>
         
                 <h1 id="title" className="title1" onClick={HomePage}>Anonymous Academy</h1>
+
+              
+                <SearchBar />
+           
 
                         <input type="checkbox" id="sidebar-active" />
                             <label for="sidebar-active" className="open-sidebar">
@@ -39,12 +46,13 @@ export default function Navigator() {
                             </label> 
 
                         <label id="overlay" for="sidebar-active"> </label>
-                        <div class="link-container"> 
+                        <div className="link-container"> 
                             <label for="sidebar-active" className="close-sidebar">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
                             </label>
-                    <SearchBar />
-                <div className="a">
+                            
+                <div id="a">
+            
                         <p className="nav-btn" onClick={SignUp}>Sign-Up</p>
                         <p className="nav-btn" onClick={Login}>Log-in</p>
                         <p className="nav-btn" onClick={NewThread}>Ask a Question!</p>
--- client/client-app/src/modules/SearchBar.js
+++ client/client-app/src/modules/SearchBar.js
@@ -1,15 +1,59 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import {ScreenContext, ScreenStateContext} from '../App.js';
 import '../App.css';
 
 export default function SearchBar() {
     const screen = useContext(ScreenContext);
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleSearch = () => {
+        setIsOpen(prevState => !prevState);
+    };
+
+    const [SearchBar, setSearchBar] = useState("search-container");
+    const [SearchBar1, setSearchBar1] = useState('');
+
+
+
+    window.addEventListener("resize", function(){
+        var w = window.innerWidth;
+
+    
+
+        if (w <= 600) {
+            setSearchBar("link-container");
+            setSearchBar1("a");
+        }else {
+            setSearchBar("search-container");
+            setSearchBar1('');
+        }
+      });
+
+
+
+
+
+
     if(screen === 0 || screen === 3) {
         return (
-            <>
-            <input type="text" placeholder="Search.." name="search"></input>
-            </>
+            <div className={SearchBar}>
+
+        <div className={`searchbar ${isOpen ? "open" : ""}`}  id={SearchBar1}>
+            <div className="box">
+                <div className={`search-box ${isOpen ? "open" : ""}`}>
+                    {isOpen && (
+                        <input type="text" placeholder="Type here..." />
+                    )}
+                    <label onClick={toggleSearch} className="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
+                            <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+                        </svg>
+                    </label>
+                </div>
+            </div>
+        </div>
+        </div>
         );
     }
 }
