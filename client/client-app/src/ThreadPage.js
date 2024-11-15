02506bc975e96d9fe106e822148843666865ac92
Matthew Gray <matthew.riley.gray@outlook.com>
2024-11-15 14:06:18 -0600

    gets thread score from server

--- client/client-app/src/ThreadPage.js
+++ client/client-app/src/ThreadPage.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Navigator from "./modules/Navigator.js";
 import ThreadReply from "./modules/ThreadReply.js";
 import ReplyList from "./modules/ReplyList.js";
@@ -6,12 +8,33 @@ import ThreadVote from "./modules/ThreadVote.js";
 import DOMPurify from "dompurify";
 
 export default function ThreadPage() {
+    const [score, findScore] = useState(0);
+
     let data = JSON.parse(sessionStorage.getItem("thread"));
 
     console.log(data);
 
     const sanitizedTitle = DOMPurify.sanitize(data[0]);
     const sanitizedDesc = DOMPurify.sanitize(data[1]);
+
+    function getCurrentVote(val) {
+        fetch("http://127.0.0.1:8000/scoreVotes", {
+            method: "POST",
+            body: JSON.stringify({thread_id:val}),
+            headers: {
+              "Content-type": "application/json; charset=UTF-8"
+            }
+          })
+            .then((response) => response.json())
+            .then((json) => giveVal(json));
+    }
+
+    function giveVal(val) {
+        console.log(val);
+        sessionStorage.setItem("thread_score", val);
+    }
+
+    getCurrentVote(data[2]);
 
     return(
         <>
@@ -22,7 +45,7 @@ export default function ThreadPage() {
                             <h1 className="thread-title" dangerouslySetInnerHTML={{__html: sanitizedTitle  }} />
                             <p className="thread-desc" dangerouslySetInnerHTML={{__html: sanitizedDesc  }} />
                             <div>
-                                <ThreadVote  Startscore={0}/>
+                                <ThreadVote />
                                 <ThreadReply />
                                 <ReplyList />
                             </div>
