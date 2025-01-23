755cb9f40b0a32bf1ab96ddc480aa05a4677ad82
Matthew Gray <matthew.riley.gray@outlook.com>
2025-01-23 14:20:50 -0600

    more data

--- server/routes/retrieveThreadData.py
+++ server/routes/retrieveThreadData.py
@@ -6,6 +6,8 @@ import sqlite3
 import uuid
 import random
 
+import routes.countViews as CV
+
 router = APIRouter()
 
 class ThreadData(BaseModel):
@@ -17,10 +19,25 @@ def sql_retrieveThread(id):
     cursor = conn.cursor()
     cursor.execute(f"SELECT * FROM threads WHERE thread_id='{id}'")
     value = cursor.fetchone()
+    print(value)
+
+    value2 = [
+        value[0],
+        value[1],
+        value[2],
+        value[4]
+    ]
+
+    cursor.execute(f"SELECT * FROM users WHERE uuid='{value[3]}'")
+    value3 = cursor.fetchone()
+
+    value2.append(f"Anonymouse {value3[4]} 1")
+
+    value2.append(CV.sql_scoreViews(id))
+    
     conn.commit()
     conn.close()
-
-    return value
+    return value2
 
 @router.post("/retrieveThread")
 def retrieveThread(data: ThreadData):
