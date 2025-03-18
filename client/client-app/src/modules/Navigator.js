074ebc163c7bdba25fd76688a7e8997eb3f59635
NotNotTrojan.jsx <70586375+Idk3333@users.noreply.github.com>
2025-03-17 23:12:17 -0500

    Update Navigator.js

--- client/client-app/src/modules/Navigator.js
+++ client/client-app/src/modules/Navigator.js
@@ -75,9 +75,33 @@ export default function Navigator() {
         <div>
             <div className="top-nav">
               <div className="top-nav-content">
-            <button className="nav-toggle" onClick={switchToggle}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
-            </button>
+            <button
+            className={`nav-toggle ${isExpanded ? 'menu-open' : ''}`}
+            onClick={switchToggle}
+          >
+            <div className="nav-toggle-open">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#5f6368"
+              >
+                <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+              </svg>
+            </div>
+            <div className="nav-toggle-close">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#5f6368"
+              >
+                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+              </svg>
+            </div>
+          </button>
                 <h1 className="title">
             <div id="hov" onClick={HomePage} isHovered={isHovered}>Anonymous Academy</div>
             </h1>
